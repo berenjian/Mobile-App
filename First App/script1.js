63ce644924835ddf62ac8e937200e7fcf4d8bcd5
Amirmahdi Brenjian <a.brenjian@1dws.com>
2012-07-23 00:47:02 +0430

    add new pages

---
+++ First App/script1.js
@@ -0,0 +1,45 @@
+// JavaScript Document// JavaScript Document
+window.onload= function(){
+	first1= document.getElementsByTagName('a').item(0);
+	
+	first1.onclick=function(){
+		alert('شما در همین صفحه هستید');
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
